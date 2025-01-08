0374e04623f995fedd6f31dcf3c4a9484cf9996b
ryzaer <ryzaer@live.com>
2025-01-08 18:02:41 +0700

    ok

--- server.php
+++ server.php
@@ -4,16 +4,16 @@ require_once('autobase.php');
 // set code , mime_type, text msg & location of temporary file
 $code = 400; 
 $mime = "text/html";
-$file = "assets/example/".uniqid();
 $text = "<i style=\"color:red\"><b>Bad Parameters</b> : The server attempted to execute incomplete data!</i>";
 $save = false;
 // init & set default bash & rule of binary name to use
 // ex. ffmpeg, pngcrush
+$file = realpath('.')."/assets/images/".uniqid();
 $rule = isset($_POST['rule']) ? $_POST['rule'] : null;
 $exec = isset($_POST['exec']) ? $_POST['exec'] : null;
 $bash = "D:/.batch/bin/$rule.exe"; // window
 $bash = "C:/Users/ditintelkam 3/$rule.exe"; // window
-$bash = "C:/Users/LENOVO/$rule.exe"; // window
+// $bash = "C:/Users/LENOVO/$rule.exe"; // window
 //$bash = "/usr/local/bin/$rule"; // linux
 
 if($rule == 'ffmpeg'){
@@ -22,15 +22,20 @@ if($rule == 'ffmpeg'){
      * https://www.johnvansickle.com/ffmpeg/ (recomanded for linux old system)
      * https://github.com/BtbN/FFmpeg-Builds/releases (all OS)
      */
-    if($exec == 'img2webp' && isset($_FILES['file']['type'])){          
+    if($exec == 'img2webp' && isset($_FILES['file']['type'])){                 
         $exts = \__fn::mime2ext($_FILES['file']['type']);
         move_uploaded_file($_FILES['file']['tmp_name'],"$file.$exts");
-        exec("$bash -i $file.$exts -c:v libwebp $file.webp");
+        exec("\"$bash\" -i \"$file.$exts\" -c:v libwebp \"$file.webp\"");
         unlink("$file.$exts");
-        $code = 200; 
-        $mime = "image/webp";
+        // $code = 200; 
+        // $mime = "image/webp";
         $file = "$file.webp";
-        $text = null;
+        // $text = null;
+        header("Content-Type:image/webp");
+        // header("Content-Disposition: inline; name=\"{$_FILES['file']['name']}\"; filename=\"{$_FILES['file']['name']}.webp\"");
+        print file_get_contents($file);
+        unlink($file);
+        die();
     }    
 } 
 
@@ -39,16 +44,20 @@ if($rule == 'pngcrush'){
      * linux : available at all repositories type unix os
      * window : https://pmt.sourceforge.io/pngcrush/
      */ 
-    $bash = "$bash -ow -rem allb -reduce"; // set yours
+    $bash = "\"$bash\" -ow -rem allb -reduce"; // set yours
     if($exec == 'img2webp' && isset($_FILES['file']['type'])){    
         $exts = \__fn::mime2ext($_FILES['file']['type']);
         if($exts == 'png'){
             move_uploaded_file($_FILES['file']['tmp_name'],"$file.$exts");
-            exec("$bash $file.$exts");
+            exec("$bash \"$file.$exts\"");
             $code = 200; 
-            $mime = "image/$exts";
+            $mime = $_FILES['file']['type'];
             $file = $file.$exts;
             $text = null;
+            header("Content-Type:image/png");
+            print file_get_contents($file);
+            unlink($file);
+            die();
         }
     }
 }
