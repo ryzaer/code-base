072302f25d529ed035c964ae67110de6bf977010
ryzaer <ryzaer@live.com>
2025-01-12 21:06:25 +0700

    up

--- mysql.json.count.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once 'autobase.php';
-$sql = db\mysql::open('root','123');
-$cmd ="select count(*) as total from db_ops.tps_data where json_extract(vote,'$.pilgub')!='[]'";
-$sql->exec($cmd);
-$prm = $sql->fetch();
-var_dump($prm);
--- mysql.tb.files.php
+++
@@ -1,6 +0,0 @@
-<?php
-include_once 'autobase.php';
-
-$db = \db\mysql::open('root','123');
-$rest = $db->name('dbserver_skck')->table('base_skck_data')->select(null,null,5);
-var_dump($rest);
