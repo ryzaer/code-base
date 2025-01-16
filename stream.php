f480d1b30da0fb34ba155b9e3d74e8ef4d8a64e7
ryzaer <ryzaer@live.com>
2025-01-16 11:25:17 +0700

    add css stream.php

--- stream.php
+++ stream.php
@@ -7,25 +7,30 @@
       <title>Document</title>
       <style>
          * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-        }
-
-        body, html {
-          width: 100%;
-          height: 100%;
-          overflow: hidden;
-        }
-        video{
-          position: absolute;
-          top: 0;
-          left: 0;
-          width: 100%;
-          height: 100%;
-          object-fit: cover;
+            margin: 0;
+            padding: 0;
+            /* box-sizing: border-box; */
          }
-        video::cue {
+         body, html {
+            width: 100%;
+            height: 100%;
+            overflow: hidden;
+         }
+         #overlay{
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+         }
+         video{
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+         }
+         video::cue {
             font-size: 75%;
             background:none;
             text-shadow: 1px 1px 2px black, 0 0 0.1em black, 0 0 0.01em black;
@@ -33,13 +38,15 @@
       </style>
    </head>
    <body>
-      <video id="video" controls preload="metadata">
-         <source src="video.php" type="video/mp4" size="720"/>
-         <source src="video.php" type="video/mp4" size="480"/>
-         <source src="video.php" type="video/mp4" size="320"/>
-         <track label="Indonesia" kind="subtitles" srclang="en" src="video.sub.php" default/>
-         <!--<track label="Deutsch" kind="subtitles" srclang="de" src="video.sub.php">
-         <track label="Español" kind="subtitles" srclang="es" src="video.sub.php">-->
-      </video>
+      <div id="overlay">
+         <video controls preload="metadata">
+            <source src="video.php" type="video/mp4" size="720"/>
+            <source src="video.php" type="video/mp4" size="480"/>
+            <source src="video.php" type="video/mp4" size="320"/>
+            <track label="Indonesia" kind="subtitles" srclang="en" src="video.sub.php" default/>
+            <!--<track label="Deutsch" kind="subtitles" srclang="de" src="video.sub.php">
+            <track label="Español" kind="subtitles" srclang="es" src="video.sub.php">-->
+         </video>
+      </div>
    </body>
 </html>
