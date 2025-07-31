2acd103725797d8fcdae3fdc8217e1747d8eb156
ryzaer <ryzaer@live.com>
2025-07-31 22:58:24 +0700

    up

--- ffmpeg.all.php
+++ ffmpeg.all.php
@@ -24,7 +24,7 @@ parse\ffmpeg::convert(function($x){
     //$folder = 'G:\@Backup_WWW\asin_ebook\edited\vidBind\@ENTITY';
     //$folder = 'C:\Action!\Video'; 
     // $folder = "D:/riza-ttnt/Videos";
-    $fname  = "092122-001";
+    $fname  = "hz-1863";
     $sname  = "$fname";
     $fmove  = "$sname";
     // $fmove = "D:/$sname";
@@ -32,7 +32,7 @@ parse\ffmpeg::convert(function($x){
     // but precission depending on fps
     // $x->mode('avi'); // params : avi, m3u8_hls, image_gif, image_webp   
     // $x->fps(30);  // mode avi will ignored if have value
-    $x->scale(720);  // mode avi will ignored if have value
+    // $x->scale(720);  // mode avi will ignored if have value
     // $x->fixtimecut("-1.5","+1.5");
     
     // $x->moveto("$folder/$fname-proc.mp4")->save();   
@@ -40,19 +40,16 @@ parse\ffmpeg::convert(function($x){
     $x->param("$folder/$sname.mp4","$fname-cut.mp4")->split([
         // dont remove this example 
         //["00:00.000","00:00.000",true,"$fname-scane-%s.mp4"],
-        // ["11:25.587","15:53.551",true,"$fmove-scane-%s.mp4"],
-        // ["47:10.832","48:11.176"],
-        // ["49:26.119","50:05.270"],
-        // ["50:52.569","51:43.539",true,"$fmove-scane-%s.mp4"],
-        // ["59:36.851","01:00:26.242"],
-        // ["01:00:43.604","01:00:58.527"],
-        // ["01:01:40.807","01:03:08.814"],
-        // ["01:03:51.742","01:05:32.875",true,"$fmove-scane-%s.mp4"],
-        // ["01:05:32.875","01:08:10.471",true,"$fmove-scane-%s.mp4"],
-        ["45:34.617","46:16.886"],
-        ["46:37.190","48:32.074"],
-        ["49:13.029","49:25.703",true,"$fmove-scane-%s.mp4"],
-        ["57:39.282","01:00:32.337",true,"$fmove-scane-%s.mp4"],
+        ["22:53.039","31:52.485",true,"$fmove-scane-%s.mp4"],
+        ["47:31.385","50:50.666"],
+        ["51:39.869","51:52.932"],
+        ["52:25.627","53:30.190"],
+        ["54:03.774","54:35.943"],
+        ["55:06.481","55:29.888"],
+        ["57:34.360","58:51.274"],
+        ["59:40.582","01:00:49.672"],
+        ["01:01:19.037","01:02:27.041",true,"$fmove-scane-%s.mp4"],
+        ["01:02:27.041","01:05:39.619",true,"$fmove-scane-%s.mp4"]
     ])->print();
     // ])->print("D:");
 });
